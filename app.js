d4cbc44d450fcd91a5df7f33961a534b8cb3d08f
Humble-Preacher <61693488+Humble-Preacher@users.noreply.github.com>
2023-02-21 02:47:58 +0000

    added functionalities

---
+++ app.js
@@ -0,0 +1,33 @@
+(function(){    
+
+    let screen = document.querySelector(".screen");
+    let buttons = document.querySelectorAll(".btn");
+    let clear = document.querySelector(".btn-clear");
+    let equal = document.querySelector(".btn-equal");
+
+    buttons.forEach(function(button){
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
+
+    equal.addEventListener('click', function(e) {
+        if (screen.value === '') {
+            screen.value = "Please enter";
+        } else {
+            try {
+                let answer = math.evaluate(screen.value);
+                screen.value = answer;
+            } catch (error) {
+                console.error(error);
+                screen.value = "Invalid Input";
+            }
+        }
+    })
+
+    clear.addEventListener('click', function(e) {
+        screen.value = '';
+    })
+
+})();
